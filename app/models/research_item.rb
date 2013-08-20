de9ea0d0856d11e313167cc66b970d9295a5dea0
Joseph Siefers <joseph.siefers@gmail.com>
2013-08-20 15:14:18 -0400

    Add relationships between research_item and user.

--- app/models/research_item.rb
+++ app/models/research_item.rb
@@ -1,2 +1,3 @@
 class ResearchItem < ActiveRecord::Base
+	belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
 	has_many :quick_ideas
 	has_many :comments
 	has_many :ideas
+	has_many :research_items
+
+	def has_research_item?(research_item)
+		research_items.include?(research_item)
+	end
 end
