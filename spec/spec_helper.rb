aac63386f45f673dd9dacbfd9c950ccdc3e68ae3
Joseph Siefers <joseph.siefers@gmail.com>
2013-08-20 13:03:05 -0400

    Change test helper to spec helper.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path('../../config/environment', __FILE__)
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+
+	config.mock_with :rspec
+	config.use_transactional_fixtures = true
+end
