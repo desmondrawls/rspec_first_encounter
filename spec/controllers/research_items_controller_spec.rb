af72f05b723da690aa79ea7d028beadb9c341f9e
Desmond Rawls <captaingrover@gmail.com>
2013-09-05 10:21:41 -0400

    pass test for assigning ID to @id

--- spec/controllers/research_items_controller_spec.rb
+++ spec/controllers/research_items_controller_spec.rb
@@ -9,10 +9,17 @@ describe ResearchItemsController do
 
       before(:each) do
         @research_item = ResearchItem.create!
+        @current_user = User.create!(:email => "example@example.com", :password => "password")
+        session[:user_id] = @current_user.id
       end
 
       it "is a valid ResearchItem" do
         expect(@research_item).to be_valid
+      end
+
+      it "assigns ID to @id" do
+        post :create, id: @research_item
+        expect(assigns(:id)).to eq @research_item.id.to_s
       end
 
     end
